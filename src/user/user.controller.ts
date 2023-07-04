2f90080ed32500ab2475bba77db555797d847ddd
nhamonin <nazarii.hamonin@gmail.com>
2023-07-04 14:24:28 +0300

    Add create user functionality to UserController and UserService

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
-@Controller('user')
-export class UserController {}
+@Controller('users')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post('register')
+  async register(@Body() createUserDto: CreateUserDto) {
+    return await this.userService.create(createUserDto);
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ConflictException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+import { instanceToPlain } from 'class-transformer';
+
+import { User } from './entities/user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const { username, email, password } = createUserDto;
+    const existingUser = await this.usersRepository.findOne({
+      where: [{ username }, { email }],
+    });
+
+    if (existingUser) {
+      throw new ConflictException('Username or email already exists');
+    }
+
+    const user = new User();
+    user.username = username;
+    user.email = email;
+    user.password = await bcrypt.hash(password, 10);
+
+    const savedUser = await this.usersRepository.save(user);
+
+    return instanceToPlain(savedUser) as User;
+  }
+}
