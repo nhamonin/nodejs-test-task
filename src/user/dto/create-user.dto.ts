607ae5bfa8c1b69033a50a8ec60a5b4f9e61b4b6
nhamonin <nazarii.hamonin@gmail.com>
2023-07-04 15:51:50 +0300

    Add global validation pipe and update user dto validation

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,13 +1,26 @@
-import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
+import {
+  IsString,
+  IsEmail,
+  IsNotEmpty,
+  MinLength,
+  MaxLength,
+} from 'class-validator';
 
 export class CreateUserDto {
-  @IsNotEmpty()
+  @IsString({ message: 'Username must be a string' })
+  @IsNotEmpty({ message: 'Username is required' })
+  @MinLength(4, { message: 'Username is too short' })
+  @MaxLength(20, { message: 'Username is too long' })
   readonly username: string;
 
-  @IsEmail()
+  @IsNotEmpty({ message: 'Email is required' })
+  @IsEmail({}, { message: 'Invalid email' })
+  @MaxLength(30, { message: 'Email is too long' })
   readonly email: string;
 
-  @IsNotEmpty()
-  @MinLength(6)
+  @IsString({ message: 'Password must be a string' })
+  @IsNotEmpty({ message: 'Password is required' })
+  @MinLength(8, { message: 'Password is too short' })
+  @MaxLength(40, { message: 'Password is too long' })
   readonly password: string;
 }
